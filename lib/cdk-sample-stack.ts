6404af32cbdeb5f72913e0771c87ba9393597bd4
amit <amit.khare4@gmail.com>
2023-08-09 21:00:30 -0400

    Example of API Gateway and Cognito

--- lib/cdk-sample-stack.ts
+++ lib/cdk-sample-stack.ts
@@ -1,16 +1,57 @@
-import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class CdkSampleStack extends cdk.Stack {
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
 
-		// The code that defines your stack goes here
+		// Create a Cognito User Pool
+		const userPool = new cognito.UserPool(this, 'MyUserPool', {
+			selfSignUpEnabled: true,
+			signInAliases: { email: true },
+			passwordPolicy: {
+				minLength: 8,
+				requireLowercase: true,
+				requireUppercase: true,
+				requireDigits: true,
+				requireSymbols: true,
+			},
+		});
 
-		// example resource
-		// const queue = new sqs.Queue(this, 'CdkSampleQueue', {
-		//   visibilityTimeout: cdk.Duration.seconds(300)
-		// });
+		// Create a Lambda function to be integrated with API Gateway
+		const handler = new lambda.Function(this, 'MyLambdaFunction', {
+			runtime: lambda.Runtime.NODEJS_18_X,
+			handler: 'index.handler',
+			code: lambda.Code.fromInline(`
+        exports.handler = async (event) => {
+          return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Hello from Lambda!' }),
+          };
+        };
+      `),
+		});
+
+		// Create an API Gateway and configure Cognito authentication for it
+		const api = new apigateway.RestApi(this, 'MyApi', {
+			defaultCorsPreflightOptions: {
+				allowOrigins: apigateway.Cors.ALL_ORIGINS,
+			},
+			defaultMethodOptions: {
+				authorizer: new apigateway.CognitoUserPoolsAuthorizer(this, 'MyCognitoAuthorizer', {
+					cognitoUserPools: [userPool],
+				}),
+			},
+		});
+
+		// Create a Lambda integration for the API Gateway
+		const lambdaIntegration = new apigateway.LambdaIntegration(handler);
+
+		// Create a resource and associate the Lambda integration with a method
+		const resource = api.root.addResource('hello');
+		resource.addMethod('GET', lambdaIntegration);
 	}
 }
--- test/app.test.ts
+++ test/app.test.ts
@@ -4,13 +4,21 @@
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/cdk-sample-stack.ts
-test('SQS Queue Created', () => {
-	//   const app = new cdk.App();
-	//     // WHEN
-	//   const stack = new CdkSample.CdkSampleStack(app, 'MyTestStack');
-	//     // THEN
-	//   const template = Template.fromStack(stack);
-	//   template.hasResourceProperties('AWS::SQS::Queue', {
-	//     VisibilityTimeout: 300
-	//   });
+test('AWS Cognito Authentication', () => {
+	// Replace 'YOUR_API_GATEWAY_ENDPOINT' with the actual URL of your API Gateway endpoint.
+	const apiEndpoint = 'https://hrq1mhy9nj.execute-api.us-east-1.amazonaws.com/prod/hello';
+
+	// Replace 'YOUR_COGNITO_ID_TOKEN' with the Cognito ID token obtained after user authentication.
+	const cognitoIdToken = '40npdr8u9k5ge4n2fljjp9gsmg';
+
+	// Make a GET request to the API Gateway endpoint with the Cognito ID token in the Authorization header.
+	fetch(apiEndpoint, {
+		method: 'GET',
+		headers: {
+			Authorization: `Bearer ${cognitoIdToken}`,
+		},
+	})
+		.then((response) => response.json())
+		.then((data) => console.log(data))
+		.catch((error) => console.error('Error:', error));
 });
